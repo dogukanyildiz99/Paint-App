476b37b687e07b3d11ce65819eea6378ef4f1bdf
Doğukan YILDIZ <dogukanyildiz99@gmail.com>
2025-01-13 18:34:37 +0300

    Line data class

--- app/src/main/java/com/example/paint/MainActivity.kt
+++ app/src/main/java/com/example/paint/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.paint
 
+import android.icu.text.ListFormatter.Width
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.DragInteraction
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -21,6 +23,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
@@ -87,3 +90,8 @@ fun BrushSizeSelector(currentSize: Float, onSizeSelected: (Float) -> Unit,
         Text("px", Modifier.align(Alignment.CenterVertically))
     }
 }
+
+data class Line(val start: Offset,
+    val end: Offset,
+    val color: Color,
+    val strokeWidth: Float = 10f)
